e2892ad98c70ce01958a6b8946af6cf6c1b35a9a
vadim l <vadimlozinschii@gmail.com>
2023-08-11 14:58:51 -0700

    seed.py

--- server/seed.py
+++ server/seed.py
@@ -3,6 +3,7 @@ from app import app
 from models import db, Category, Activity
 
 categories = {
+    #Focus on activities that have a direct physical impact or are related to body care.
     "Physical": 
     [
         {"name": "Exercise", "description": "Engage in physical activity to boost mood and health."},
@@ -16,8 +17,14 @@ categories = {
         {"name": "Eat Right", "description": "Consume a balanced diet for physical health and mental well-being."},
         {"name": "Drink Tea", "description": "Sip on herbal teas that can calm the nervous system."},
         {"name": "Stay Hydrated", "description": "Drink water regularly for physical and mental well-being."},
+        {"name": "Limit Light at Night", "description": "Maintain a dark environment for better sleep."},
+        {"name": "Go to Bed Early", "description": "Prioritize rest to rejuvenate mind and body."},
+        {"name": "Wake Up Early", "description": "Start the day with a fresh perspective."},
+        {"name": "Listen to Your Body", "description": "Tune into physical cues for overall well-being."},
+        {"name": "Treat Yourself", "description": "Indulge in a treat or activity to boost mood."}
     ],
 
+    #Activities related to mental well-being, thinking patterns, and cognitive tasks.
     "Mental": 
     [
         {"name": "Meditate", "description": "Practice mindfulness to calm the mind and reduce stress."},
@@ -36,8 +43,11 @@ categories = {
         {"name": "Challenge Negative Thoughts", "description": "Reframe unhelpful patterns of thinking."},
         {"name": "Learn Something New", "description": "Engage the brain and expand knowledge."},
         {"name": "Do Puzzles", "description": "Challenge the brain and focus on a task."},
+        {"name": "Look for the Good", "description": "Seek positivity in every situation."},
+        {"name": "Be Yourself", "description": "Embrace authenticity and individuality."},
     ],
 
+    #Activities that focus on emotional well-being and self-awareness
     "Emotional": 
     [
         {"name": "Laugh", "description": "Seek humor to lighten mood and reduce stress."},
@@ -56,8 +66,10 @@ categories = {
         {"name": "Engage in a Hobby", "description": "Spend time on a personal interest or passion."},
         {"name": "Listen to Music", "description": "Enjoy melodies to relax and elevate mood."},
         {"name": "Take Breaks", "description": "Pause regularly to recharge during work or tasks."},
+        {"name": "Take a Vacation or Day Trip", "description": "Change scenery to break routine and refresh."}
     ],
 
+    #Activities related to social interaction, community engagement, and relationships.
     "Social": 
     [
         {"name": "Spend Time with Friends or Family", "description": "Connect with loved ones for emotional support."},
@@ -78,20 +90,22 @@ categories = {
         {"name": "Use Essential Oils", "description": "Engage the senses with calming or invigorating scents."},
     ],
 
+    #Activities related to nutrition.
     "Nutritional": 
     [
-        {"name": "Limit Light at Night", "description": "Maintain a dark environment for better sleep."},
-        {"name": "Go to Bed Early", "description": "Prioritize rest to rejuvenate mind and body."},
-        {"name": "Wake Up Early", "description": "Start the day with a fresh perspective."},
         {"name": "Eat Right", "description": "Consume a balanced diet for physical health and mental well-being."},
-        {"name": "Look for the Good", "description": "Seek positivity in every situation."},
         {"name": "Limit Stimulants", "description": "Reduce intake of substances that can increase stress."},
-        {"name": "Take a Vacation or Day Trip", "description": "Change scenery to break routine and refresh."},
-        {"name": "Treat Yourself", "description": "Indulge in a treat or activity to boost mood."},
-        {"name": "Be Yourself", "description": "Embrace authenticity and individuality."},
         {"name": "Listen to Your Body", "description": "Tune into physical cues for overall well-being."},
+        {"name": "Limit Caffeine", "description": "Reduce caffeine to prevent jitters and anxiety."},
+        {"name": "Avoid Sugar", "description": "Limit sugar intake to prevent mood swings."},
+        {"name": "Stay Hydrated", "description": "Drink water regularly for physical and mental well-being."},
+        {"name": "Consume Omega-3 Fatty Acids", "description": "Eat foods rich in omega-3s for brain health."},
+        {"name": "Eat More Greens", "description": "Incorporate more vegetables into your diet for essential nutrients."},
+        {"name": "Limit Processed Foods", "description": "Avoid foods with added preservatives and chemicals."},
+        {"name": "Treat Yourself", "description": "Indulge in a treat or activity to boost mood, but in moderation."},
     ]
 }
+
 
 if __name__ == '__main__':
     with app.app_context():

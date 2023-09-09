3c726937476637e0142b09a684bfd07311c8eb6a
vadim l <vadimlozinschii@gmail.com>
2023-09-08 18:08:27 -0700

    started work on scroll manager, working towards single page layout. updated tailwindconfig colors

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,32 +1,97 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-function Home() {
-  return (
-    <div className="relative min-h-screen flex flex-col">
-      {/* About Us Section */}
-      <motion.section 
-        className="flex flex-col items-center justify-center flex-1 bg-gray-800 text-white p-8"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-      >
-        <h1 className="text-4xl font-bold mb-4">About Us</h1>
-        <p className="text-lg leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu gravida suscipit. Integer auctor urna vitae leo hendrerit, nec cursus lacus laoreet.</p>
-      </motion.section>
-      
-      {/* Goals Section */}
-      <motion.section 
-        className="flex flex-col items-center justify-center flex-1 bg-gray-900 text-white p-8"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 0.5 }}
-      >
-        <h1 className="text-4xl font-bold mb-4">Our Goals</h1>
-        <p className="text-lg leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex eu gravida suscipit. Integer auctor urna vitae leo hendrerit, nec cursus lacus laoreet.</p>
-      </motion.section>
-    </div>
-  );
+const Section = (props) => {
+    const {children} = props;
+
+    return (
+        <motion.section 
+            className="h-screen w-full p-4 max-w-screen-2xl mx-auto flex flex-col item-start justify-center"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.4 } }}
+        >
+            {children}
+        </motion.section>
+    )
 }
 
+const AboutUs = () => {
+    return (
+        <Section id="aboutUsSection">
+            <h1 className="text-6xl font-extrabold leading-snug">
+                About Us
+            </h1>
+            <motion.p 
+                className="text-lg mt-4 font-medium"
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.8 } }}
+            >
+                Your description about "About Us" goes here. 
+            </motion.p>
+        </Section>
+    )
+}
+
+const Goals = () => {
+    return (
+        <Section id="goalsSection">
+            <h1 className="text-6xl font-extrabold leading-snug">
+                Our Goals
+            </h1>
+            <motion.p 
+                className="text-lg mt-4 font-medium"
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.8 } }}
+            >
+                Your description about "Goals" goes here.
+            </motion.p>
+        </Section>
+    )
+}
+
+const RelaxationTechniques = () => {
+    return (
+        <Section id="relaxationTechniquesSection">
+            <h1 className="text-6xl font-extrabold leading-snug">
+                Relaxation Techniques
+            </h1>
+            <motion.p 
+                className="text-lg mt-4 font-medium"
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.8 } }}
+            >
+                Your description about "Relaxation Techniques" goes here.
+            </motion.p>
+        </Section>
+    )
+}
+
+const StressManagementActivities = () => {
+    return (
+        <Section id="stressManagementActivitiesSection">
+            <h1 className="text-6xl font-extrabold leading-snug">
+                Stress Management Activities
+            </h1>
+            <motion.p 
+                className="text-lg mt-4 font-medium"
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.8 } }}
+            >
+                Your description about "Stress Management Activities" goes here.
+            </motion.p>
+        </Section>
+    )
+}
+
+const Home = () => (
+  <div className="flex flex-col items-center w-screen">
+      <AboutUs />
+      <Goals />
+      <RelaxationTechniques />
+      <StressManagementActivities />
+  </div>
+);  
+
 export default Home;
+
+
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
 
-export const Navbar = (props) => {
-    const { onSectionChange } = props;
+export const Navbar = () => {
     const [NavbarBtnOpened, setNavbarBtnOpened] = useState(false);
 
     return (
         <>
             <button 
             onClick={() => setNavbarBtnOpened(!NavbarBtnOpened)}
-            className="drop-shadow-md fixed top-12 right-12 p-3 text-2xl z-20 w-11 h-11 rounded-md bg-orange-600">
+            className="drop-shadow-md fixed top-12 right-12 p-3 text-2xl z-20 w-11 h-11 rounded-md bg-orange">
                 <div
                     className={`bg-white h-0.5 rounded-md w-full transition-all ${
                         NavbarBtnOpened ? "rotate-45 translate-y-0.5" : ""
@@ -26,30 +26,30 @@ export const Navbar = (props) => {
                 />
             </button>
             <div
-                className={`z-10 fixed top-0 right-0 bottom-0 bg-green-600
+                className={`z-10 fixed top-0 right-0 bottom-0 bg-lightgreen
                             overflow-hidden transition-all flex flex-col
                 ${NavbarBtnOpened ? "w-80" : "w-0"}`}
             >
                 <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
-                    <NavbarBtnBtn label="About Us" onClick={() => onSectionChange(0)} />
-                    <NavbarBtnBtn label="Goals" onClick={() => onSectionChange(1)} />
-                    <NavbarBtnBtn label="Relaxation Techniques" onClick={() => onSectionChange(2)} />
-                    <NavbarBtnBtn label="Stress Management Activities" onClick={() => onSectionChange(3)} />
+                    <NavbarBtn label="About Us" to="aboutUsSection" />
+                    <NavbarBtn label="Goals" to="goalsSection" />
+                    <NavbarBtn label="Relaxation Techniques" to="relaxationTechniquesSection" />
+                    <NavbarBtn label="Stress Management Activities" to="stressManagementActivitiesSection" />
                 </div>
             </div>
         </>
     );
 }
 
-const NavbarBtnBtn = (props) => {
-    const { label, onClick } = props;
-
+const NavbarBtn = ({ label, to }) => {
     return (
-        <button
-            onClick={onClick}
-            className="drop-shadow-md opacity-85 text-orange-600 text-2xl font-bold cursor-pointer hover:text-white transition-colors"
+        <ScrollLink
+            to={to}
+            smooth={true}
+            duration={500}
+            className="drop-shadow-md opacity-85 text-orange text-2xl font-bold cursor-pointer hover:text-white transition-colors"
         >
             {label}
-        </button>
+        </ScrollLink>
     );
 }

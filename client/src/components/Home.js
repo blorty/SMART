05aba5239ab905a015b756e5b645a395131875b6
vadim l <vadimlozinschii@gmail.com>
2023-09-09 18:36:45 -0700

    more styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,14 +7,14 @@ const Section = (props) => {
     const {children, id} = props;
 
     return (
-        <motion.section 
-            id={id}
-            className="h-screen w-full p-4 max-w-screen-2xl mx-auto flex flex-col item-start justify-center bg-mattebrown"
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.25, delay: 0.25 } }}
-        >
-            {children}
-        </motion.section>
+    <motion.section 
+        id={id}
+        className="h-screen w-full p-4 md:p-4 sm:p-2 p-1 max-w-screen-2xl mx-auto flex flex-col item-start justify-center bg-mattebrown"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0, transition: { duration: 0.25, delay: 0.25 } }}
+    >
+        {children}
+    </motion.section>
     )
 }
 
@@ -22,7 +22,7 @@ const Section = (props) => {
 const AboutUs = () => {
     return (
         <Section id="aboutUsSection">
-            <h1 className="text-6xl font-extrabold leading-snug ">
+            <h1 className="text-6xl md:text-6xl sm:text-5xl font-extrabold leading-snug">
                 About Us
             </h1>
             <motion.p 
@@ -39,7 +39,7 @@ const AboutUs = () => {
 const Goals = () => {
     return (
         <Section id="goalsSection">
-            <h1 className="text-6xl font-extrabold leading-snug">
+            <h1 className="text-6xl md:text-6xl sm:text-5xl font-extrabold leading-snug">
                 Our Goals
             </h1>
             <motion.p 
@@ -56,7 +56,7 @@ const Goals = () => {
 const RelaxationTechniques = () => {
     return (
         <Section id="relaxationTechniquesSection">
-            <h1 className="text-6xl font-extrabold leading-snug">
+            <h1 className="text-6xl md:text-6xl sm:text-5xl font-extrabold leading-snug">
                 Relaxation Techniques
             </h1>
             <motion.p 
@@ -73,7 +73,7 @@ const RelaxationTechniques = () => {
 const StressManagementActivities = () => {
     return (
         <Section id="stressManagementActivitiesSection">
-            <h1 className="text-6xl font-extrabold leading-snug">
+            <h1 className="text-6xl md:text-6xl sm:text-5xl font-extrabold leading-snug">
                 Stress Management Activities
             </h1>
             <motion.p 
@@ -88,6 +88,7 @@ const StressManagementActivities = () => {
 }
 
 const Home = () => (
+
     <motion.div 
         id='logo' 
         className="flex flex-col items-center w-screen relative"
@@ -97,10 +98,14 @@ const Home = () => (
     >
     <Link 
         to="/" 
-        className="hover:text-lightgreen drop-shadow-lg absolute top-0 left-1/2 transform -translate-x-1/2 mt-4 py-2 px-4 font-bold cursor-pointer z-50"
+        className="hover:text-lightgreen drop-shadow-lg absolute left-1/2 transform -translate-x-1/2 md:mt-5 sm:mt-2 mt-1 font-bold cursor-pointer z-50"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.25, delay: 0.2 }}
     >
         SMART
     </Link>
+
     
     
     <AboutUs />

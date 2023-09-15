69f19c6cf0cf11f8a3df5e3c2450c6ed3e516917
vadim l <vadimlozinschii@gmail.com>
2023-09-15 14:38:36 -0700

    added randomizer

--- client/src/components/CardSection.js
+++ client/src/components/CardSection.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { AppContext } from '../AppContext'; // Adjusted the path
 import Card from './Card';  // Adjusted the path to point to the correct Card.js file
 import { AnimatePresence } from 'framer-motion';  // Removed the unused 'motion' import
@@ -6,31 +6,42 @@ import { AnimatePresence } from 'framer-motion';  // Removed the unused 'motion'
 const CardSection = () => {
     const { relaxationTechniques } = useContext(AppContext);
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [shuffledTechniques, setShuffledTechniques] = useState([]);
 
-    const flattenedRelaxationTechniques = relaxationTechniques.flatMap(subcategory => 
-        subcategory.activities.map(activity => ({
-            ...activity, 
-            subCategoryName: subcategory.name,
-            mainCategoryName: "Relaxation Techniques"
-        }))
-    );
+    useEffect(() => {
+        const flattenAndShuffle = (arr) => {
+            let flatArr = arr.flatMap(subcategory => 
+                subcategory.activities.map(activity => ({
+                    ...activity, 
+                    subCategoryName: subcategory.name,
+                    mainCategoryName: "Relaxation Techniques"
+                }))
+            );
+            
+            for (let i = flatArr.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [flatArr[i], flatArr[j]] = [flatArr[j], flatArr[i]];
+            }
+
+            return flatArr;
+        };
+
+        setShuffledTechniques(flattenAndShuffle(relaxationTechniques));
+    }, [relaxationTechniques]);
 
     const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % flattenedRelaxationTechniques.length);
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % shuffledTechniques.length);
     };
 
     const handlePrev = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + flattenedRelaxationTechniques.length) % flattenedRelaxationTechniques.length);
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + shuffledTechniques.length) % shuffledTechniques.length);
     };
 
     return (
-        <div className="flex items-center justify-center margin-auto">
+        <div className="flex items-center">
             <button onClick={handlePrev} className="p-4 bg-gray-300 rounded-md">Previous</button>
             <AnimatePresence>
-                {flattenedRelaxationTechniques.length > 0 
-                    && 
-                        <Card key={currentIndex} activity={flattenedRelaxationTechniques[currentIndex]} 
-                        handleNext={handleNext} handlePrev={handlePrev} />}
+                {shuffledTechniques.length > 0 && <Card key={currentIndex} activity={shuffledTechniques[currentIndex]} handleNext={handleNext} handlePrev={handlePrev} />}
             </AnimatePresence>
             <button onClick={handleNext} className="p-4 bg-gray-300 rounded-md">Next</button>
         </div>
